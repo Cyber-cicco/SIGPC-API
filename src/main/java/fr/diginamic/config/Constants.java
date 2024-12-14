92aaf87c450468f24658291f28e4cb55434450d1
Gildas Montcho <gmontcho@gmail.com>
2024-12-15 00:44:16 +0100

    feat(config/): ajouter 2 classes de configuration

---
+++ src/main/java/fr/diginamic/config/Constants.java
@@ -0,0 +1,11 @@
+package fr.diginamic.config;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
+
+public class Constants {
+  public static final String API_VERSION_1 = "/api/v1";
+
+  public static final DateTimeFormatter DEFAULT_DATE_FORMATTER =
+      DateTimeFormatter.ofPattern("uuuu-MM-dd").withResolverStyle(ResolverStyle.STRICT);
+}
