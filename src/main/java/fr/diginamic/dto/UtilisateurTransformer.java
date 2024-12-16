228a70a7bd20fb458a27282eeba8aa71ac6b9afb
Gildas Montcho <gmontcho@gmail.com>
2024-12-16 22:10:26 +0100

    fix: ajouter corrections pom.xml et autres corrections mineures

--- src/main/java/fr/diginamic/dto/UtilisateurTransformer.java
+++ src/main/java/fr/diginamic/dto/UtilisateurTransformer.java
@@ -1,6 +1,5 @@
 package fr.diginamic.dto;
 
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Component;
 import fr.diginamic.entities.Utilisateur;
@@ -10,41 +9,45 @@ import java.util.UUID;
 @Component
 public class UtilisateurTransformer {
 
-    public Utilisateur fromCompteToUtilisateur(CompteDto compteDto, String password, String roles, UUID activationLink) throws JsonProcessingException {
-        return Utilisateur.builder()
-                .nom(compteDto.getNom())
-                .prenom(compteDto.getPrenom())
-                .password(password)
-                .activationLink(activationLink)
-                .roles(roles)
-                .email(compteDto.getEmail())
-                .emailVerified(false)
-                .build();
-    }
-    public UtilisateurDto toUtilisateurDto(Utilisateur entity){
-        UtilisateurDto dto = new UtilisateurDto();
-        dto.setId(entity.getId());
-        dto.setEmail(entity.getEmail());
-        dto.setNom(entity.getNom());
-        dto.setPrenom(entity.getPrenom());
-        dto.setEmailVerified(entity.isEmailVerified());
-        dto.setRoles(entity.getRoles());
-        dto.setDateEcheanceSuppression(entity.getDateEcheanceSuppression());
+  public Utilisateur fromCompteToUtilisateur(
+      CompteDto compteDto, String password, String roles, UUID activationLink)
+      throws JsonProcessingException {
+    return Utilisateur.builder()
+        .nom(compteDto.getNom())
+        .prenom(compteDto.getPrenom())
+        .password(password)
+        .activationLink(activationLink)
+        .roles(roles)
+        .email(compteDto.getEmail())
+        .emailVerified(false)
+        .build();
+  }
 
-        return dto;
-    }      
+  public UtilisateurDto toUtilisateurDto(Utilisateur entity) {
+    UtilisateurDto dto = new UtilisateurDto();
+    dto.setId(entity.getId());
+    dto.setEmail(entity.getEmail());
+    dto.setNom(entity.getNom());
+    dto.setPrenom(entity.getPrenom());
+    dto.setActivationLink(entity.getActivationLink().toString());
+    dto.setEmailVerified(entity.isEmailVerified());
+    dto.setRoles(entity.getRoles());
+    dto.setDateEcheanceSuppression(entity.getDateEcheanceSuppression());
 
-    public Utilisateur toutilisateur(UtilisateurDto dto){
-        Utilisateur entity = new Utilisateur();
-        entity.setId(dto.getId());
-        entity.setEmail(dto.getEmail());
-        entity.setNom(dto.getNom());
-        entity.setPrenom(dto.getPrenom());
-        entity.setEmailVerified(dto.isEmailVerified());
-        entity.setRoles(dto.getRoles());
-        entity.setDateEcheanceSuppression(dto.getDateEcheanceSuppression());
+    return dto;
+  }
 
-        //TODO : implémenter les méthodes pour les champs complexes
-        return entity;
-    }      
+  public Utilisateur toutilisateur(UtilisateurDto dto) {
+    Utilisateur entity = new Utilisateur();
+    entity.setId(dto.getId());
+    entity.setEmail(dto.getEmail());
+    entity.setNom(dto.getNom());
+    entity.setPrenom(dto.getPrenom());
+    entity.setEmailVerified(dto.isEmailVerified());
+    entity.setRoles(dto.getRoles());
+    entity.setDateEcheanceSuppression(dto.getDateEcheanceSuppression());
+
+    // TODO : implémenter les méthodes pour les champs complexes
+    return entity;
+  }
 }
