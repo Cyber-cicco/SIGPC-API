e2743fd821979b75439a115f9888161f3ef90b93
Gildas Montcho <gmontcho@gmail.com>
2024-12-16 11:07:02 +0100

    feat(projet/): entité Projet: create, getById, getAll

---
+++ src/main/java/fr/diginamic/projet/ProjetDto.java
@@ -0,0 +1,118 @@
+package fr.diginamic.projet;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import fr.diginamic.dto.CommentaireDto;
+import fr.diginamic.dto.EquipeDto;
+import fr.diginamic.dto.EvenementDto;
+import fr.diginamic.dto.ProjetUtilisateurDto;
+import fr.diginamic.dto.RessourceDto;
+import fr.diginamic.dto.SprintDto;
+import fr.diginamic.validation.CustomLocalDate;
+import fr.diginamic.validation.annotations.EndDateAfterStartDate;
+import fr.diginamic.validation.deserializers.LocalDateDeserializer;
+import jakarta.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+// @EndDateAfterStartDateStr.List({
+//  @EndDateAfterStartDateStr(
+//      startDateField = "dateDebut",
+//      endDateField = "dateFin",
+//      message = "La date de fin doit être après la date de début"),
+//  @EndDateAfterStartDateStr(
+//      startDateField = "dateDebut",
+//      endDateField = "dateFinEstimee",
+//      message = "La date de fin estimée doit être après la date de début")
+// })
+
+@EndDateAfterStartDate.List({
+  @EndDateAfterStartDate(
+      startDateField = "dateDebut",
+      endDateField = "dateFin",
+      message = "La date de fin doit être après la date de début"),
+  @EndDateAfterStartDate(
+      startDateField = "dateDebut",
+      endDateField = "dateFinEstimee",
+      message = "La date de fin estimée doit être après la date de début")
+})
+public class ProjetDto {
+
+  private Long id;
+
+  @NotNull(message = "Le nom du projet ne doit pas être null")
+  @NotBlank(message = "Le nom du projet ne doit pas être vide")
+  @Size(min = 5, max = 100, message = "Le nom du projet doit avoir entre 5 et 100 caractères")
+  @UniqueProjectName
+  private String nom;
+
+  @Size(max = 512, message = "La description doit faire moins de 512 caractères")
+  private String description;
+
+  //  @NotNull(message = "La date de début est requise")
+  //  @StringToLocalDate
+  //  private String dateDebut;
+
+  @NotNull(message = "La date de début est requise")
+  //  @JsonFormat(pattern = "uuuu-MM-dd")
+  @JsonDeserialize(using = LocalDateDeserializer.class)
+  @CustomLocalDate
+  private LocalDate dateDebut;
+
+  private LocalDate dateFin;
+
+  private LocalDate dateFinEstimee;
+
+  //  private String dateFin;
+
+  //  private String dateFinEstimee;
+
+  //
+  //  @JsonFormat(pattern = "yyyy-MM-dd")
+  //  private LocalDate dateFin;
+  //
+  //  @JsonFormat(pattern = "yyyy-MM-dd")
+  //  private LocalDate dateFinEstimee;
+
+  @NotNull(message = "L'email de contact est requis")
+  @Email(message = "Le contact doit être un e-mail valide")
+  private String contact;
+
+  private List<CommentaireDto> commentaires;
+  private EquipeDto equipe;
+  private List<EvenementDto> evenements;
+  private List<SprintDto> sprint;
+  private List<RessourceDto> ressources;
+  private List<ProjetUtilisateurDto> projetUtilisateurs;
+
+  @Override
+  public String toString() {
+    return "ProjetDto{"
+        + "nom='"
+        + nom
+        + '\''
+        + ", description='"
+        + description
+        + '\''
+        + ", dateDebut='"
+        + dateDebut
+        + '\''
+        + ", dateFin='"
+        + dateFin
+        + '\''
+        + ", dateFinEstimee='"
+        + dateFinEstimee
+        + '\''
+        + ", contact='"
+        + contact
+        + '\''
+        + '}';
+  }
+}
---
+++ src/main/java/fr/diginamic/projet/ProjetService.java
@@ -0,0 +1,45 @@
+package fr.diginamic.projet;
+
+import fr.diginamic.exception.ResourceNotFoundException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProjetService implements ProjetServiceInterface {
+
+  private final ProjetRepository projetRepository;
+  private final ProjetTransformer projetTransformer;
+  private final ModelMapper modelMapper;
+
+  public List<ProjetDto> getAllProjets() {
+    List<Projet> projetsDansDb = projetRepository.findAll();
+    List<ProjetDto> projetsDtos;
+    projetsDtos =
+        projetsDansDb.stream().map(projet -> modelMapper.map(projet, ProjetDto.class)).toList();
+    return projetsDtos;
+  }
+
+  public ProjetDto getProjetById(Long projetId) {
+    Projet projetDansDb =
+        projetRepository
+            .findById(projetId)
+            .orElseThrow(() -> new ResourceNotFoundException("Projet", "id", projetId));
+    return modelMapper.map(projetDansDb, ProjetDto.class);
+  }
+
+  public ProjetDto createProjet(ProjetDto projetDto) {
+    Projet projet = modelMapper.map(projetDto, Projet.class);
+    //    Projet savedProjet = projetTransformer.toProjet(projetDto);
+    //    Projet savedProjet = projetRepository.findByNom(projet.getNom());
+    //    if (savedProjet != null) {
+    //      throw new ApiException("Un projet avec le nom '" + projetDto.getNom() + "' existe
+    // déjà");
+    //    }
+    Projet savedProjet = projetRepository.save(projet);
+    return modelMapper.map(savedProjet, ProjetDto.class);
+    //    return projetTransformer.toProjetDto(savedProjet);
+  }
+}
