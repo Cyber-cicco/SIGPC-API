7112460ded416982a553dafbf19dd9f934380b24
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-12-17 15:55:17 +0100

    Méthode pour créer un sprint

--- src/main/java/fr/diginamic/controller/SprintController.java
+++ src/main/java/fr/diginamic/controller/SprintController.java
@@ -2,21 +2,45 @@ package fr.diginamic.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;    
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import fr.diginamic.dto.SprintDto;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import fr.diginamic.services.SprintService;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/sprint")
 public class SprintController {
 
-   
     private final SprintService sprintService;
+
+    /**
+     * Créer un sprint
+     * 
+     * Informations à mettre dans le body :
+     * - dateDebut
+     * - dateFin
+     * - sprintParJour (optionnel)
+     * - projetId (optionnel)
+     * - userStories (optionnel)
+     * 
+     * @param sprintDto
+     * @return
+     */
+    @PostMapping("/creer")
+    public ResponseEntity<SprintDto> creerSprint(@RequestBody SprintDto sprintDto) {
+        if (sprintDto.getDateDebut() == null || sprintDto.getDateFin() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Date de début et de fin obligatoires");
+        }
+        SprintDto sprint = sprintService.creerSprint(sprintDto);
+        return ResponseEntity.ok(sprint);
+    }
 
 }
--- src/main/java/fr/diginamic/services/SprintService.java
+++ src/main/java/fr/diginamic/services/SprintService.java
@@ -1,16 +1,23 @@
 package fr.diginamic.services;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import fr.diginamic.dto.SprintDto;
 import fr.diginamic.dto.SprintTransformer;
+import fr.diginamic.entities.Sprint;
 import fr.diginamic.repository.SprintRepository;
 
-import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class SprintService {
 
     private final SprintRepository sprintRepository;
     private final SprintTransformer sprintTransformer;
+
+    public SprintDto creerSprint(SprintDto sprintDto) {
+        Sprint sprint = sprintTransformer.tosprint(sprintDto);
+        Sprint savedSprint = sprintRepository.save(sprint);
+        return sprintTransformer.tosprintDto(savedSprint);
+    }
 
 }
